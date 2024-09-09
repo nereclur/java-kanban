e6a125e6b5d92497086a2a7d7c71556c8948fd32
dmitry <nereclur@yandex.ru>
2024-09-09 12:35:38 +0300

    Переделал логику работы истории

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -3,10 +3,13 @@ package manager;
 import task.Task;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface HistoryManager {
 
     void add(Task task);
 
     List<Task> getHistory();
+
+
 }
