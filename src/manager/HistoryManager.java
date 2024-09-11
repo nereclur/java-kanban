764b6f418911da509997eb4c9155bfbd0f027d67
nereclur <nereclur@yandex.ru>
2024-09-11 15:59:35 +0300

    Revert "Переделал логику работы истории"

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -3,13 +3,10 @@ package manager;
 import task.Task;
 
 import java.util.List;
-import java.util.Objects;
 
 public interface HistoryManager {
 
     void add(Task task);
 
     List<Task> getHistory();
-
-
 }
