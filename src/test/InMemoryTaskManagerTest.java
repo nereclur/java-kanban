2de5d9801e05244fe39e631d4174c4ec577e3b2b
dmitry <nereclur@yandex.ru>
2024-09-18 12:06:56 +0300

    Удалил неиспользуемый импорт InMemHistManager

--- src/test/InMemoryTaskManagerTest.java
+++ src/test/InMemoryTaskManagerTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import manager.InMemoryTaskManager;
-import manager.InMemoryHistoryManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import task.Epic;
