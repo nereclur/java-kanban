6f0ae471f322e6978d79828ce00f2b2b6195fefe
dmitry <nereclur@yandex.ru>
2024-09-20 09:13:58 +0300

    Добавил импорты

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import manager.InMemoryTaskManager;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
