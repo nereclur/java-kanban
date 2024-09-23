dfc87daf21933106c8398944436fe659a10c57bb
dmitry <nereclur@yandex.ru>
2024-09-23 12:04:04 +0300

    Реализовал два метода taskFromString и loadFromFile

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -3,6 +3,8 @@ package manager;
 import exceptions.FileWriterSaveException;
 import task.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
@@ -10,6 +12,28 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     public FileBackedTaskManager(File file) {
         this.file = file;
+    }
+
+    public static FileBackedTaskManager loadFromFile(File file) {
+        FileBackedTaskManager manager = new FileBackedTaskManager(file);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                Task task = taskFromString(line);
+                if (task.getType().equals("EPIC")) {
+                    manager.createEpic((Epic) task);
+                } else if (task.getType().equals("SUBTASK")) {
+                    manager.createSubtask((Subtask) task);
+                } else {
+                    manager.createTask(task);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return manager;
     }
 
     private void save() {
@@ -32,6 +56,36 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         }
     }
 
+    private static Task taskFromString(String line) {
+        String[] fields = line.split(",");
+
+        int id = Integer.parseInt(fields[0]);
+        String type = fields[1];
+        String name = fields[2];
+        String description = fields[4];
+
+        Task task = null;
+
+        switch (type) {
+            case "TASK":
+                task = new Task(name, description);
+                break;
+            case "EPIC":
+                task = new Epic(name, description);
+                break;
+            case "SUBTASK":
+                int epicId = Integer.parseInt(fields[5]);
+                task = new Subtask(name, description, epicId);
+                break;
+        }
+
+        if (task != null) {
+            task.setTaskId(id);
+        }
+
+        return task;
+    }
+
     private String taskToString(Task task) {
         StringBuilder sb = new StringBuilder();
         sb.append(task.getTaskId()).append(",");
@@ -50,4 +104,81 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return task.getType().name();
     }
 
+    @Override
+    public void createTask(Task task) {
+        super.createTask(task);
+        save();
+    }
+
+    @Override
+    public void clearAllTask() {
+        super.clearAllTask();
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task, int id) {
+        super.updateTask(task, id);
+        save();
+    }
+
+    @Override
+    public void removeTaskById(int id) {
+        super.removeTaskById(id);
+        save();
+    }
+
+    @Override
+    public void createEpic(Epic epic) {
+        super.createEpic(epic);
+        save();
+    }
+
+    @Override
+    public void clearAllEpic() {
+        super.clearAllEpic();
+        save();
+    }
+
+    @Override
+    public void updateEpic(int id, String name, String description) {
+        super.updateEpic(id, name, description);
+        save();
+    }
+
+    @Override
+    public void removeEpicById(int id) {
+        super.removeEpicById(id);
+        save();
+    }
+
+    @Override
+    public void createSubtask(Subtask subtask) {
+        super.createSubtask(subtask);
+        save();
+    }
+
+    @Override
+    public void clearAllSubtask() {
+        super.clearAllSubtask();
+        save();
+    }
+
+    @Override
+    public void updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+        save();
+    }
+
+    @Override
+    public void removeSubtaskById(int id) {
+        super.removeSubtaskById(id);
+        save();
+    }
+
+    @Override
+    public void updateEpicStatus(int id) {
+        super.updateEpicStatus(id);
+        save();
+    }
 }
---
+++ src/manager/FileBackedTaskManagerTest.java
@@ -0,0 +1,38 @@
+package manager;
+
+import task.Epic;
+import task.Subtask;
+import task.Task;
+
+
+import java.io.File;
+
+
+
+public class FileBackedTaskManagerTest {
+    public static void main(String[] args) {
+        File data = new File("data.csv");
+        FileBackedTaskManager manager = new FileBackedTaskManager(data);
+
+        Task task = new Task("Task1", "Description1");
+        Task task2 = new Task("Task2", "Description2");
+        manager.createTask(task);
+        manager.createTask(task2);
+
+        Epic epic = new Epic("Epic1", "Description1");
+        manager.createEpic(epic);
+
+        Subtask subtask = new Subtask("Subtask1", "Description1", epic.getTaskId());
+        manager.createSubtask(subtask);
+
+        System.out.println("Tasks saved to file: ");
+        manager.getAllTask().forEach(System.out::println);
+        manager.getAllEpics().forEach(System.out::println);
+
+        // Загружаем из файла
+        FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(data);
+        System.out.println("Tasks loaded from file: ");
+        loadedManager.getAllTask().forEach(System.out::println);
+        loadedManager.getAllEpics().forEach(System.out::println);
+    }
+}
