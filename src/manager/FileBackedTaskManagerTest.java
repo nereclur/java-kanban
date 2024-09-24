08be7053ef16f2171d8bc8eead330c5f97dd5b30
dmitry <nereclur@yandex.ru>
2024-09-24 04:58:25 +0300

    удалил неиспользуемые импорты

--- src/manager/FileBackedTaskManagerTest.java
+++ src/manager/FileBackedTaskManagerTest.java
@@ -3,31 +3,30 @@ package manager;
 import task.Epic;
 import task.Subtask;
 import task.Task;
-import task.TypeTask;
 
 import java.io.File;
 
 public class FileBackedTaskManagerTest {
     public static void main(String[] args) {
         File data = new File("data.csv");
-//        FileBackedTaskManager manager = new FileBackedTaskManager(data);
-//
-//        // Создание задач
-//        Task task1 = new Task("Task1", "Description1");
-//        Task task2 = new Task("Task2", "Description2");
-//        manager.createTask(task1);
-//        manager.createTask(task2);
-//
-//        Epic epic = new Epic("Epic1", "Description1");
-//        manager.createEpic(epic);
-//
-//        Subtask subtask = new Subtask("Subtask1", "Description1", epic.getTaskId());
-//        manager.createSubtask(subtask);
-//
-//        System.out.println("Tasks saved to file: ");
-//        manager.getAllTask().forEach(System.out::println);
-//        manager.getAllEpics().forEach(System.out::println);
-//        manager.getAllSubtask().forEach(System.out::println);
+        FileBackedTaskManager manager = new FileBackedTaskManager(data);
+
+        // Создание задач
+        Task task1 = new Task("Task1", "Description1");
+        Task task2 = new Task("Task2", "Description2");
+        manager.createTask(task1);
+        manager.createTask(task2);
+
+        Epic epic = new Epic("Epic1", "Description1");
+        manager.createEpic(epic);
+
+        Subtask subtask = new Subtask("Subtask1", "Description1", epic.getTaskId());
+        manager.createSubtask(subtask);
+
+        System.out.println("Tasks saved to file: ");
+        manager.getAllTask().forEach(System.out::println);
+        manager.getAllEpics().forEach(System.out::println);
+        manager.getAllSubtask().forEach(System.out::println);
 
         // Загружаем из файла
         FileBackedTaskManager loadedManager = FileBackedTaskManager.loadFromFile(data);
